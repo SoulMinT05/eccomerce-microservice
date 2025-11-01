84358b9904e94ed658a74d99fe83ba9b45115377
SoulMinT05 <minhtam5111993@gmail.com>
2025-11-01 15:07:28 +0700

    add cart page, react-hook-form with zod for shipping form and payment form

--- apps/client/src/components/Header.tsx
+++ apps/client/src/components/Header.tsx
@@ -3,8 +3,9 @@ import Link from 'next/link';
 import React from 'react';
 
 import SearchBar from './SearchBar';
-import { Bell, Home, ShoppingCart } from 'lucide-react';
+import { Bell, Home } from 'lucide-react';
 import logo from '../../public/logo.png';
+import ShoppingCartIcon from './ShoppingCartIcon';
 
 const Header = () => {
     return (
@@ -21,7 +22,7 @@ const Header = () => {
                     <Home className="w-4 h-4 text-gray-600" />
                 </Link>
                 <Bell className="w-4 h-4 text-gray-600" />
-                <ShoppingCart className="w-4 h-4 text-gray-600" />
+                <ShoppingCartIcon />
                 <Link href="/login">Sign in</Link>
             </div>
         </header>
